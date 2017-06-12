4db8130ec8c08f19fad82d492ee566bfdec12f76
dreamer3c <2978425474@qq.com>
2017-06-12 09:02:51 +0800

    dreamer3c
    primes

---
+++ two/prime.h
@@ -0,0 +1,14 @@
+//
+//  prime.h
+//  two
+//
+//  Created by 20161104579 on 2017/6/12.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#ifndef prime_h
+#define prime_h
+
+#include <stdio.h>
+#include <math.h>
+#endif /* prime_h */
